6af8f833c6b3cab1f354ce377362f036bfdf1035
Sadat Chowdhury <sadat.c.25@gmail.com>
2025-03-07 23:37:38 -0500

    script changes

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,13 +1,9 @@
-// Flip the image twice when the page loads
+// flips an image 2 times horizonally
 window.onload = function() {
     const image = document.getElementById("flipImage");
-
-    // First flip (after 0.5s)
     setTimeout(() => {
         image.classList.add("scale-x-[-1]");
     }, 500);
-
-    // Second flip (after 1s)
     setTimeout(() => {
         image.classList.remove("scale-x-[-1]");
     }, 1000);
